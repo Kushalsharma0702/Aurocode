d47ce5310afba9f07493845607c76d22852fcc5e
Kushal sharma <146510678+Kushalsharma0702@users.noreply.github.com>
2024-07-03 05:07:48 +0530

    Update SingleFeature.tsx

--- src/components/Features/SingleFeature.tsx
+++ src/components/Features/SingleFeature.tsx
@@ -11,9 +11,11 @@ const SingleFeature = ({ feature }: { feature: Feature }) => {
         <h3 className="mb-5 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
           {title}
         </h3>
-        <p className="pr-[10px] text-base font-medium leading-relaxed text-body-color">
-          {paragraph}
-        </p>
+        {paragraph && (
+          <p className="pr-[10px] text-base font-medium leading-relaxed text-body-color">
+            {paragraph}
+          </p>
+        )}
       </div>
     </div>
   );
